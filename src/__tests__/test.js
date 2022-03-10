dd47646c6c538ed025290a63dcb02bf3606f852c
EscapedGibbon <maximgolubev2@gmail.com>
2022-03-10 03:54:49 +0100

    refactor: multiple tests combined and called "general"

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,16 +1,12 @@
 import isPrime from '../index';
 
 describe('verify isPrime', () => {
-  it('testingSix', () => {
+  it('general', () => {
     expect(isPrime(6)).toStrictEqual(false);
-  });
-  it('testingFifteen', () => {
     expect(isPrime(15)).toStrictEqual(false);
-  });
-
-  it('testingTwo', () => {
     expect(isPrime(2)).toStrictEqual(true);
   });
+
   it('testingOne', () => {
     expect(isPrime(1)).toStrictEqual(false);
   });
